51507729ca25314d3aeaa9271e2896c94714381c
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2025-05-13 21:59:36 +0600

    added file upload route for dp

--- src/app/routes/authRoutes.ts
+++ src/app/routes/authRoutes.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import { AuthController } from '../controllers/authController';
 import protect from '../middlewares/authMiddleware';
+import upload from '../middlewares/uploadMiddleware';
 
 
 const router = express.Router();
 
 router.post('/login', AuthController.loginUser);
 router.post('/register', AuthController.registerUser);
-router.get('/profile',protect,AuthController.getUserProfile);
+router.get('/profile', protect, AuthController.getUserProfile);
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
+    res.status(200).json({ imageUrl });
+})
 
 export const AuthRoutes = router;
